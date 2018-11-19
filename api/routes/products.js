e51f2de5248d1e3726e409cce8177e5b7bb87937
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-19 21:58:47 +0530

    Adding db connectivity

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,43 +1,86 @@
 const express = require('express')
-
+const Product = require('../models/product')
 const productsrouter = express.Router();
+const mongoose = require('mongoose')
 
 productsrouter.get('/', (req,resp,next) => {
-    resp.status(200).json({
-        'message': 'Handling GET request'
+    Product.find().exec()
+    .then(result => {
+        resp.status(200).json(result)
+    })
+    .catch(err => {
+        resp.status(500).json(err);
     })
 } )
 
 productsrouter.post('/', (req,resp,next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    }
-    resp.status(201).json({
-        message: 'Handling POST request',
-        createdProduct: product
+    const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      resp.status(201).json({
+        message: "Handling POST requests to /products",
+        createdProduct: result
+      });
     })
+    .catch(err => {
+      console.log(err);
+      resp.status(500).json({
+        error: err
+      });
+    });
 } )
 
 productsrouter.get('/:id', (req,resp,next) => {
-    const msg = 'Handling GET request with id ' + req.params.id;
-    resp.status(200).json({
-        'message': msg
+    Product.findById(req.params.id).exec()
+    .then(result => {
+        if(result)
+            resp.status(200).json(result)
+        else
+            resp.status(404).json('No matching id found')
+
+    })
+    .catch(err => {
+        resp.status(500).json(err);
     })
 } )
 
 productsrouter.patch('/:id', (req,resp,next) => {
-    const msg = 'Handling PATCH request with id ' + req.params.id;
-    resp.status(200).json({
-        'message': msg
-    })
+    const product = new Product({
+        _id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+      });
+    product.isNew = false;
+    product.save().then(result => {
+        console.log(result);
+        resp.status(201).json({
+          message: "Handling PATCH requests to /products",
+          createdProduct: result
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        resp.status(500).json({
+          error: err
+        });
+      });
 } )
 
 productsrouter.delete('/:id', (req,resp,next) => {
     const msg = 'Handling DELETE request with id ' + req.params.id;
-    resp.status(200).json({
-        'message': msg
-    })
+    Product.remove({_id: req.params.id})
+            .then(result => {
+                resp.status(200).json('Deleted the record with id ' + req.params.id)
+            })
+            .catch(err => {
+                resp.status(500).json(err);
+            })
 } )
 
 module.exports = productsrouter;
--- app.js
+++ app.js
@@ -2,9 +2,29 @@ const express = require('express')
 const productsrouter = require('./api/routes/products')
 const ordersrouter = require('./api/routes/orders')
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
 var cors=require('cors');
+var mongoose = require('mongoose');
 
+var uri = 'mongodb://arnab:Nov@2018@nodeapicluster-shard-00-00-zaytg.mongodb.net:27017,nodeapicluster-shard-00-01-zaytg.mongodb.net:27017,nodeapicluster-shard-00-02-zaytg.mongodb.net:27017/test?ssl=true&replicaSet=NodeApiCluster-shard-0&authSource=admin&retryWrites=true'
+
+// mongoose.connect(uri,{ useNewUrlParser: true })
+mongoose.connect(
+    'mongodb+srv://nodeapicluster-zaytg.mongodb.net/test?retryWrites=true',
+    {
+      auth: {
+        user: 'arnab',
+        password: 'Nov@2018'
+      },
+      useNewUrlParser: true
+    },
+    function(err, client) {
+      if (err) {
+        console.log(err);
+      }
+      else
+        console.log('connected!!!');
+    }
+  );
 
 const app = express()
 
