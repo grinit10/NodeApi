b91bfd7770badd9ea44a4247c5fbed3871263b04
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-19 16:12:11 +0530

    Adding nodemon, error handling and logging

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const productsrouter = require('./api/routes/products')
 const ordersrouter = require('./api/routes/orders')
+const morgan = require('morgan')
 
 
 const app = express();
-// app.use((req, resp, next) => {
-//     resp.status(200).json({
-//         message: 'It works!'
-//     })
-// });
 
+app.use(morgan('dev'))
 app.use('/products', productsrouter)
 app.use('/orders', ordersrouter)
+app.use((req,resp,next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req,resp,next) => {
+    resp.status(err.status || 500);
+    resp.json({
+        'message': err.message
+    })
+})
 
 module.exports = app;
